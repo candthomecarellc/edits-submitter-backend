de492d5b689bc2e62245c81169b6cefd21ec7f34
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2025-02-13 22:42:35 +0600

    No changes made.

--- src/app/modules/Form/from.service.ts
+++ src/app/modules/Form/from.service.ts
@@ -18,113 +18,26 @@ const takeAndProcessData = async (
     form.getTextField('applicantName').setText(data.applicantName || '');
     form.getTextField('applicationDate').setText(data.applicationDate || '');
     
-    // Check the checkbox US Citizen DOB
-    if(data.uscitizenshiporDOB === 'US passportcard') form.getCheckBox('US passportcard').check();
-    if(data.uscitizenshiporDOB === 'Certificate of Naturalization DHS Forms N550 or N570') form.getCheckBox('Certificate of Naturalization DHS Forms N550 or N570').check();
-    if(data.uscitizenshiporDOB === 'Certificate of US Citizenship DHS Forms N560 or N561') form.getCheckBox('Certificate of US Citizenship DHS Forms N560 or N561').check();
-    if(data.uscitizenshiporDOB === 'NYS Enhanced Drivers License EDL') form.getCheckBox('NYS Enhanced Drivers License EDL').check();
-    if(data.uscitizenshiporDOB === 'Native American Tribal Document issued by a Federally Recognized Tribe') form.getCheckBox('Native American Tribal Document issued by a Federally Recognized Tribe').check();
-    
-    // Check the checkbox US Citizen
-    if(data.usCitizenship === 'US Birth Certificate') form.getCheckBox('US Birth Certificate').check();
-    if(data.usCitizenship === 'Certification of Birth issued by Department of State Forms FS545 or DS1350') form.getCheckBox('Certification of Birth issued by Department of State Forms FS545 or DS1350').check();
-    if(data.usCitizenship === 'US Citizen ID Card Form I197') form.getCheckBox('US Citizen ID Card Form I197').check();
-    if(data.usCitizenship === 'Report of Birth Abroad FS240') form.getCheckBox('Report of Birth Abroad FS240').check();
-    if(data.usCitizenship === 'US National ID card Form I197 or I179') form.getCheckBox('US National ID card Form I197 or I179').check();
-    if(data.usCitizenship === 'ReligiousSchool Records') form.getCheckBox('ReligiousSchool Records').check();
-    if(data.usCitizenship === 'Military record of service showing US place of birth') form.getCheckBox('Military record of service showing US place of birth').check();
-    if(data.usCitizenship === 'Final adoption decree') form.getCheckBox('Final adoption decree').check();
-    if(data.usCitizenship === 'Evidence of qualifying for US citizenship under the Child Citizenship Act of 2000') form.getCheckBox('Evidence of qualifying for US citizenship under the Child Citizenship Act of 2000').check();
-    
-    // Check the checkbox identityDocument
-    if(data.identityDocument === 'State Drivers license or ID card with photo') form.getCheckBox('State Drivers license or ID card with photo').check();
-    if(data.identityDocument === 'ID card issued by a federal state or local government agency') form.getCheckBox('ID card issued by a federal state or local government agency').check();
-    if(data.identityDocument === 'US Military card or draft record or US Coast Guard Merchant Mariner Card') form.getCheckBox('US Military card or draft record or US Coast Guard Merchant Mariner Card').check();
-    if(data.identityDocument === 'School ID card with a photo may also show date of birth') form.getCheckBox('School ID card with a photo may also show date of birth').check();
-    if(data.identityDocument === 'Certificate of Degree of Indian blood or other American IndianAlaska Native tribal document') form.getCheckBox('Certificate of Degree of Indian blood or other American IndianAlaska Native tribal document').check();
-    if(data.identityDocument === 'Verified School Nursery or Daycare records for children under 18') form.getCheckBox('Verified School Nursery or Daycare records for children under 18').check();
-    if(data.identityDocument === 'Clinic Doctor or Hospital records for children under 18') form.getCheckBox('Clinic Doctor or Hospital records for children under 18').check();
-
-    // Check the checkbox identityDocumentExtra
-    if(data.identityDocumentExtra === 'Marriage certificate') form.getCheckBox('Marriage certificate').check();
-    if(data.identityDocumentExtra === 'NYS Benefit Identification Card') form.getCheckBox('NYS Benefit Identification Card').check();
-
-
+    if(data.uscitizenshiporDOB) form.getCheckBox(data.uscitizenshiporDOB).check();
+    if(data.usCitizenship) form.getCheckBox(data.usCitizenship).check();
+    if(data.identityDocument) form.getCheckBox(data.identityDocument).check();
+    if(data.identityDocumentExtra) form.getCheckBox(data.identityDocumentExtra).check();
     if(data.ImmigrationstatesIdentity) form.getCheckBox(data.ImmigrationstatesIdentity).check();
-
-
     if(data.ImmigrationStatus) form.getCheckBox(data.ImmigrationStatus).check();
-
-    // Check the checkbox dobIdentity
-    if(data.dobIdentity === 'Visa') form.getCheckBox('Visa').check();
-    if(data.dobIdentity === 'US Passport') form.getCheckBox('US Passport').check();
-
-    // Check the checkbox homeAddressDocument
-    if(data.homeAddressDocument === 'Lease letter rent receipt with your home address from landlord') form.getCheckBox('Lease letter rent receipt with your home address from landlord').check();
-    if(data.homeAddressDocument === 'Utility Bill gas electric phone cable fuel or water') form.getCheckBox('Utility Bill gas electric phone cable fuel or water').check();
-    if(data.homeAddressDocument === 'Property tax records or mortgage statement') form.getCheckBox('Property tax records or mortgage statement').check();
-    if(data.homeAddressDocument === 'Drivers license if issued in the past 6 months') form.getCheckBox('Drivers license if issued in the past 6 months').check();
-    if(data.homeAddressDocument === 'Government ID card with address') form.getCheckBox('Government ID card with address').check();
-    if(data.homeAddressDocument === 'Postmarked envelope or post card cannot use if sent to a PO Box') form.getCheckBox('Postmarked envelope or post card cannot use if sent to a PO Box').check();
-
-    // Check the checkbox wagesAndSalary
-    if(data.wagesAndSalary === 'Paycheck stubs') form.getCheckBox('Paycheck stubs').check();
-    if(data.wagesAndSalary === 'Letter from employer on company') form.getCheckBox('Letter from employer on company').check();
-    if(data.wagesAndSalary === 'Businesspayroll records') form.getCheckBox('Businesspayroll records').check();
-
-    // Check the checkbox selfEmployment
-    if(data.selfEmployment === 'Current signed and dated income tax return') form.getCheckBox('Current signed and dated income tax return').check();
-    if(data.selfEmployment === 'Records of earnings and expenses') form.getCheckBox('Records of earnings and expenses').check();
-    
-    // Check the checkbox unemploymentBenefits
-    if(data.unemploymentBenefits === 'Award lettercertificate') form.getCheckBox('Award lettercertificate').check();
-    if(data.unemploymentBenefits === 'Monthly benefit statement from NYS') form.getCheckBox('Monthly benefit statement from NYS').check();
-    if(data.unemploymentBenefits === 'Printout of recipients account information') form.getCheckBox('Printout of recipients account information').check();
-    if(data.unemploymentBenefits === 'Copy of Direct Payment Card with printout') form.getCheckBox('Copy of Direct Payment Card with printout').check();
-    if(data.unemploymentBenefits === 'Correspondence from the NYS Department') form.getCheckBox('Correspondence from the NYS Department').check();
-
-    // Check the checkbox privatePensionsAnnuities
-    if(data.privatePensionsAnnuities === 'tatement from pensionannuity') form.getCheckBox('tatement from pensionannuity').check();
-   
-    // Check the checkbox socialSecurity
-    if(data.socialSecurity === 'Award lettercertificate_2') form.getCheckBox('Award lettercertificate_2').check();
-    if(data.socialSecurity === 'Annual benefit statement') form.getCheckBox('Annual benefit statement').check();
-    if(data.socialSecurity === 'Correspondence from Social Security') form.getCheckBox('Correspondence from Social Security').check();
-  
-    // Check the checkbox workersCompensation
-    if(data.workersCompensation === 'Award letter') form.getCheckBox('Award letter').check();
-    if(data.workersCompensation === 'Check stub') form.getCheckBox('Check stub').check();
-
-    // Check the checkbox childSupportAlimony
-    if(data.childSupportAlimony === 'Letter from person providing support') form.getCheckBox('Letter from person providing support').check();
-    if(data.childSupportAlimony === 'Letter from court') form.getCheckBox('Letter from court').check();
-    if(data.childSupportAlimony === 'Child supportalimony check stub') form.getCheckBox('Child supportalimony check stub').check();
-    if(data.childSupportAlimony === 'Copy of NY EPPICard with printout') form.getCheckBox('Copy of NY EPPICard with printout').check();
-    if(data.childSupportAlimony === 'Copy of child support account information') form.getCheckBox('Copy of child support account information').check();
-    if(data.childSupportAlimony === 'Copy of bank statement showing') form.getCheckBox('Copy of bank statement showing').check();
-
-    // Check the checkbox veteransBenefits
-    if(data.veteransBenefits === 'Award letter_2') form.getCheckBox('Award letter_2').check();
-    if(data.veteransBenefits === 'Benefit check stub') form.getCheckBox('Benefit check stub').check();
-    if(data.veteransBenefits === 'Correspondence from Veterans Affairs') form.getCheckBox('Correspondence from Veterans Affairs').check();
-
-    // Check the checkbox militaryPay
-    if(data.militaryPay === 'Award letter_3') form.getCheckBox('Award letter_3').check();
-    if(data.militaryPay === 'Check stub_2') form.getCheckBox('Check stub_2').check();
-
-    // Check the checkbox incomeFromRent
-    if(data.incomeFromRent === 'Letter from roomer boarder tenant') form.getCheckBox('Letter from roomer boarder tenant').check();
-    if(data.incomeFromRent === 'Check stub_3') form.getCheckBox('Check stub_3').check();
-
-    // Check the checkbox interestDividendsRoyalties
-    if(data.interestDividendsRoyalties === 'Recent statement from bank credit union') form.getCheckBox('Recent statement from bank credit union').check();
-    if(data.interestDividendsRoyalties === 'Letter from broker') form.getCheckBox('Letter from broker').check();
-    if(data.interestDividendsRoyalties === 'Letter from agent') form.getCheckBox('Letter from agent').check();
-    if(data.interestDividendsRoyalties === '1099 or tax return if no other') form.getCheckBox('1099 or tax return if no other').check();
-
-    // Check the checkbox careForChildrenorAdults
-    if(data.careForChildrenorAdults === 'Written statement from day care center or other childadult care provider') form.getCheckBox('Written statement from day care center or other childadult care provider').check();
-    if(data.careForChildrenorAdults === 'Canceled checks or receipts that show your payments') form.getCheckBox('Canceled checks or receipts that show your payments').check();
+    if(data.dobIdentity) form.getCheckBox(data.dobIdentity).check();
+    if(data.homeAddressDocument) form.getCheckBox(data.homeAddressDocument).check();
+    if(data.wagesAndSalary) form.getCheckBox(data.wagesAndSalary).check();
+    if(data.selfEmployment) form.getCheckBox(data.selfEmployment).check();
+    if(data.unemploymentBenefits) form.getCheckBox(data.unemploymentBenefits).check();
+    if(data.privatePensionsAnnuities) form.getCheckBox(data.privatePensionsAnnuities).check();
+    if(data.socialSecurity) form.getCheckBox(data.socialSecurity).check();
+    if(data.workersCompensation) form.getCheckBox(data.workersCompensation).check();
+    if(data.childSupportAlimony) form.getCheckBox(data.childSupportAlimony).check();
+    if(data.veteransBenefits) form.getCheckBox(data.veteransBenefits).check();
+    if(data.militaryPay) form.getCheckBox(data.militaryPay).check();
+    if(data.incomeFromRent) form.getCheckBox(data.incomeFromRent).check();
+    if(data.interestDividendsRoyalties) form.getCheckBox(data.interestDividendsRoyalties).check();
+    if(data.careForChildrenorAdults) form.getCheckBox(data.careForChildrenorAdults).check();
 
     /*---------- TODO: Have to Add Checkbox of courtOrdered from file ----------------*/
     /*---------- TODO: Have to Add Checkbox healthInsurance.proofOfCurrentHealthInsurance from file ------------*/
@@ -142,10 +55,7 @@ const takeAndProcessData = async (
     /*---------- TODO: Have to Add Checkbox resources.deedForRealEstate from file ------------*/
 
     // Check the checkbox proofOfstudentStatus
-    if(data.proofOfstudentStatus === 'Copy of schedule') form.getCheckBox('Copy of schedule').check();
-    if(data.proofOfstudentStatus === 'Statement from college or university') form.getCheckBox('Statement from college or university').check();
-    if(data.proofOfstudentStatus === 'Other correspondence from college showing student status') form.getCheckBox('Other correspondence from college showing student status').check();  
-
+    if(data.proofOfstudentStatus) form.getCheckBox(data.proofOfstudentStatus).check();
 
     form.getTextField('personalInfo.firstName').setText(data?.['personalInfo.firstName'] || '');
     form.getTextField('personalInfo.middleName').setText(data?.['personalInfo.middleName'] || '');
